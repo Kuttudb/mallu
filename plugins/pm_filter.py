150e93fd44ea4984efa11ae7daa9fd36f2ab5a3b
Kuttudb <92996439+Kuttudb@users.noreply.github.com>
2023-01-01 00:19:21 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -254,28 +254,34 @@ async def next_page(bot, query):
 @Client.on_callback_query(filters.regex("^spolling"))
 async def advantage_spoll_choker(bot, query):
     _, user, movie_ = query.data.split('#')
-    movies = SPELL_CHECK.get(query.message.reply_to_message)
-    if not movies:
-        return await query.answer(script.OLD_ALRT_TXT.format(query.from_user.first_name), show_alert=True)
     if int(user) != 0 and query.from_user.id != int(user):
-        return await query.answer(script.ALRT_TXT.format(query.from_user.first_name), show_alert=True)
+        return await query.answer("This is not for you", show_alert=True)
     if movie_ == "close_spellcheck":
         return await query.message.delete()
-    movie = movies[(int(movie_))]
-    await query.answer(script.TOP_ALRT_MSG)
+    try:
+        movies = SPELL_CHECK.get(query.message.reply_to_message.id)
+    except Exception:
+        return await query.answer('Something went wrong...')
+    if not movies:
+        return await query.answer("You are clicking on an old button which is expired.", show_alert=True)
+
+    movie = movies[int(movie_)]
+    await query.answer('Checking for Movie in database...')
     k = await manual_filters(bot, query.message, text=movie)
     if k == False:
-        files, offset, total_results = await get_search_results(query.message.chat.id, movie, offset=0, filter=True)
+        files, offset, total_results = await get_search_results(movie, offset=0, filter=True)
+
         if files:
-            k = (movie, files, offset, total_results)
+            k = movie, files, offset, total_results
             await auto_filter(bot, query, k)
         else:
-            reqstr1 = query.from_user.id if query.from_user else 0
-            reqstr = await bot.get_users(reqstr1)
-            await bot.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, movie)))
-            k = await query.message.edit(script.MVE_NT_FND)
-            await asyncio.sleep(10)
-            await k.delete()
+            try:
+                k = await query.message.edit('This Movie Not Found In DataBase. \nTry Request Again with correct spelling')
+
+                await asyncio.sleep(10)
+                await k.delete()
+            except Exception:
+                return
 
 
 @Client.on_callback_query()
