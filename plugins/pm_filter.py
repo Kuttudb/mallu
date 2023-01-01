f74693c2b1977a0357e8269c06da0b363c3f01d3
Kuttudb <92996439+Kuttudb@users.noreply.github.com>
2023-01-01 16:14:46 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -61,7 +61,7 @@ async def pm_text(bot, message):
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
     if user_id in ADMINS: return # ignore admins
-    await message.reply_text("<b>Iғ Yᴏᴜ Aʀᴇ ᴀʟᴏᴏᴋɪɴɢ Fᴏʀ Mᴏᴠɪᴇs / Sᴇʀɪᴇs sᴇᴀʀᴄʜ ɪᴛ ᴏɴ 🔍 @Mallufiles_group . \n Tʜɪs ᴍᴇssᴀɢᴇ ʜᴀs ʙᴇᴇɴ sᴇɴᴛ ᴛᴏ Tʜᴇ ᴍᴏᴅᴇʀᴀᴛᴏʀs !</b>")
+    await message.reply_text("<b>Yᴏᴜʀ ᴍᴇssᴀɢᴇ ʜᴀs ʙᴇᴇɴ sᴇɴᴛ ᴛᴏ ᴍʏ ᴍᴏᴅᴇʀᴀᴛᴏʀs !</b>")
     await bot.send_message(
         chat_id=LOG_CHANNEL,
         text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
