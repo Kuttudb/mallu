bbd0b603c696dea33c2bf8dbc1fb34387f83a772
Kuttudb <92996439+Kuttudb@users.noreply.github.com>
2023-01-01 14:03:21 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -37,7 +37,7 @@ BUTTONS = {}
 SPELL_CHECK = {}
 
 @Client.on_message(filters.private & filters.text & filters.incoming)
-async def pm_text(client, message):
+async def pm_text(bot, message):
     content = message.text
     user = message.from_user.first_name
     user_id = message.from_user.id
