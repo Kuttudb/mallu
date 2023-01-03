479fc6bd2621c12b3fd2f1d19179267d167d4a3e
Kuttudb <92996439+Kuttudb@users.noreply.github.com>
2023-01-03 11:56:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -271,7 +271,7 @@ async def advantage_spoll_choker(bot, query):
             reqstr1 = query.from_user.id if query.from_user else 0
             reqstr = await bot.get_users(reqstr1)
             await bot.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, movie)))
-            await query.reply_photo(
+            await query.reply_to_message(
             photo=SPELL_IMG, 
             caption=script.I_CUDNT.format(mv_rqst)
         )
