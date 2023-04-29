b92b02205f2cc637d100d0db6102dbaa2bc8d731
Kuttudb <92996439+Kuttudb@users.noreply.github.com>
2023-04-29 12:45:03 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -52,6 +52,7 @@ async def start(client, message):
                     InlineKeyboardButton('🔰 Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ 🔰', url=CHNL_LNK)
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
+        
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
