f171ef53aed42b3a03416db742cbe2a36c06d508
Kuttudb <92996439+Kuttudb@users.noreply.github.com>
2023-04-29 12:58:25 +0530

    Update commands.py sticker

--- plugins/commands.py
+++ plugins/commands.py
@@ -52,7 +52,13 @@ async def start(client, message):
                     InlineKeyboardButton('🔰 Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ 🔰', url=CHNL_LNK)
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        
+        await message.reply_chat_action(enums.ChatAction.TYPING)
+
+        m=await message.reply_sticker("CAACAgUAAxkBAAIFNGJSlfOErbkSeLt9SnOniU-58UUBAAKaAAPIlGQULGXh4VzvJWoeBA")
+
+        await asyncio.sleep(1)
+
+        await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
